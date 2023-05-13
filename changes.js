4dce95b695259d7094dc01e1f8a8f61215c9042d
Denis Gamzikov <gamzikovd@yandex.ru>
2023-05-13 22:52:55 +0300

    all phone, no full menu

--- changes.js
+++ changes.js
@@ -1,11 +1,10 @@
-const removeClass = (aEl) => {
-  aEl.classList.remove("active");
+const removeClass = (linkEl) => {
+  linkEl.classList.remove("active");
 };
 const ActiveButton = (id) => {
   location.hash = id;
-  console.log({ id });
-  const allaEl = document.querySelectorAll(".menu_button a");
-  allaEl.forEach(removeClass);
+  const linksEl = document.querySelectorAll(".navigation-low-width-screen a");
+  linksEl.forEach(removeClass);
   const aEL = document.getElementById(id);
   aEL.classList.add("active");
 };
