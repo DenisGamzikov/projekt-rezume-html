84ae0b377e42761c14ea3f9fab4606af01a87780
Denis Gamzikov <gamzikovd@yandex.ru>
2023-05-14 19:24:24 +0300

    phone-full

--- changes.js
+++ changes.js
@@ -1,10 +1,21 @@
+const body = document.getElementById("body");
+const menuEl = document.getElementById("navigation-low-width-screen");
 const removeClass = (linkEl) => {
   linkEl.classList.remove("active");
 };
 const ActiveButton = (id) => {
   location.hash = id;
-  const linksEl = document.querySelectorAll(".navigation-low-width-screen a");
+  const linksEl = document.querySelectorAll(".low-width-menu-active a");
   linksEl.forEach(removeClass);
+  body.classList.remove("unactive-body");
+  menuEl.classList.remove("low-width-menu-active");
+  menuEl.classList.add("navigation-low-width-screen");
   const aEL = document.getElementById(id);
   aEL.classList.add("active");
 };
+
+function openMenu() {
+  menuEl.classList.remove("navigation-low-width-screen");
+  menuEl.classList.add("low-width-menu-active");
+  body.classList.add("unactive-body");
+}
