1b3ac26dd4e9ff335aca42e783c4ade1d36bf3a5
Denis Gamzikov <gamzikovd@yandex.ru>
2023-05-27 19:48:45 +0300

    all-work

--- changes.js
+++ changes.js
@@ -1,20 +1,40 @@
 const body = document.getElementById("body");
 const menuEl = document.getElementById("navigation-low-width-screen");
-const removeClass = (linkEl) => {
-  linkEl.classList.remove("active");
-};
+const screenEl = document.getElementById("all-screen");
+
 const ActiveButton = (id) => {
-  const linksEl = document.querySelectorAll(".low-width-menu-active a");
-  linksEl.forEach(removeClass);
   body.classList.remove("unactive-body");
   menuEl.classList.remove("low-width-menu-active");
   menuEl.classList.add("navigation-low-width-screen");
-  const aEL = document.getElementById(id);
-  aEL.classList.add("active");
+  screenEl.classList.remove("opacity-el");
 };
 
 function openMenu() {
   menuEl.classList.remove("navigation-low-width-screen");
   menuEl.classList.add("low-width-menu-active");
   body.classList.add("unactive-body");
+  screenEl.classList.add("opacity-el");
 }
+const bigPicture = document.getElementById("container-big-picture");
+
+function willBig() {
+  bigPicture.classList.remove("full-picture-off");
+  bigPicture.classList.add("full-picture-on");
+  screenEl.classList.add("display-none");
+}
+const closeButtonEl = document.getElementById("close-gray-square");
+function willSmall() {
+  bigPicture.classList.add("full-picture-off");
+  bigPicture.classList.remove("full-picture-on");
+  screenEl.classList.remove("display-none");
+}
+const imgEl = document.getElementById("big-whatsapp-logo");
+const btnEl = document.getElementById("pop-up-button");
+imgEl.onmouseout = function () {
+  btnEl.classList.add("unactive-button");
+  btnEl.classList.remove("active-button");
+};
+imgEl.onmouseover = function () {
+  btnEl.classList.remove("unactive-button");
+  btnEl.classList.add("active-button");
+};
